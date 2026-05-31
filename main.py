d1e75f0432e0f168eb8f43a3d57adc2a1777e426
terminalcrazy1 <terminalcrazy1@gmail.com>
2026-05-31 09:56:43 -0400

    add setup to main.py

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ import json
 import discord
 import discord.ext
 
+class WipeBot(commands.bot):
+    def __init__(self):
+        super().__init__(
+            command_prefix="/", 
+            intents=discord.Intents.default(),
+            intents.members=True
+            )
+    
+    async def setup_hook(self):
+        await self.tree.sync()
+        
