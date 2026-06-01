4124f031d78b6ee23966a79754bc821c58fdacbe
terminalcrazy1 <terminalcrazy1@gmail.com>
2026-06-01 17:52:51 -0400

    add wipe-end schedule clear

--- wipe_end.py
+++ wipe_end.py
@@ -7,6 +7,11 @@ eligible_role = json.load(open("config.json"))["ELIGIBLE_ROLE"]
 wipe_role = json.load(open("config.json"))["WIPE_ROLE"]
 
 async def wipe_end_back(interaction: discord.Interaction):
+    data = open("data.json", "w")
+    data_dict = {"WIPE_SCHEDULE": ""}
+    json.dump(data_dict, data)
+    data.close()
+
     role = discord.utils.get(interaction.guild.roles, name=f"{wipe_role}")
     await interaction.response.send_message(content=f"{role.mention} the wipe has ended. See you next time!")
     for member in role.members:
